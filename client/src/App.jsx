a3dd51218c711cad5c83fc8d8c28ed94db1bb4a3
Manisangh Sharma <manisanghsharma@gmail.com>
2023-12-02 10:23:12 +0530

    Made the app responsive

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,8 +12,7 @@ function App() {
 	}, [notes]);
 
 	return (
-		<>
-			<div className='px-32 pt-16 max-md:px-7'>
+			<div className='px-32 pt-16 max-lg:px-16 max-sm:px-7 max-sm:pt-7'>
 				<h1 className='flex items-center gap-2 text-5xl font-semibold text-white'>
 					<img src='assets/notes.png' className='w-20' />
 					Notes
@@ -29,7 +28,6 @@ function App() {
 					</p>
 				)}
 			</div>
-		</>
 	);
 }
 
